6267ca4f6ff400fcf6994242afecaf92ca2d3938
Grace Aewmu <pinklipznoni@gmail.com>
2024-04-10 17:34:27 +0000

    backend

---
+++ backend/scripts/deploy.ts
@@ -0,0 +1,22 @@
+import hre from "hardhat";
+
+
+async function main() {
+
+    const Erc20 = await hre.ethers.getContractFactory("ERC20");
+    const erc20 = await Erc20.deploy();
+
+    await erc20.waitForDeployment();
+
+    console.log(
+        `Token deployed to  ${erc20.target}`
+    );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
+
